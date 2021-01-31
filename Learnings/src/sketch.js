7816f8948168ca12f8f488b8e7819829a19a0a4d
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-31 21:29:53 +0600

    Basics of p5js

---
+++ Learnings/src/sketch.js
@@ -0,0 +1,18 @@
+function setup()
+{
+    createCanvas(400,300)
+}
+
+function draw()
+{
+    background(255,204,0)
+    
+    line(0,0,400,300)
+  
+    rectMode(RADIUS);
+    fill(255);
+    rect(180, 138, 60, 50);
+    
+    
+    
+}
