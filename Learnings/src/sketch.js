4732a8a2d719497bb27a5df7482c588ef7df557a
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-01 22:14:53 +0600

    Use of random function

--- Learnings/src/sketch.js
+++ Learnings/src/sketch.js
@@ -1,7 +1,12 @@
-
-var r=0;
-var g=0;
-var b=0;
+var a=0;
+var b=400;
+var c=0;
+var d=400;
+var e=0;
+var f=0;
+var g=400;
+var h=400;
+var i=400;
 var x=0;
 var y=400;
 function setup() {
@@ -9,15 +14,35 @@ function setup() {
 }
 
 function draw() {
-  r=map(mouseX,0,400,0,255);
-  g=map(mouseY,0,400,255,0);
-  b=map(mouseX,0,400,255,0);
-  background(r,g,b);
   
-  fill(255,0,0);
+  background(255);
+  
+  fill(random(0,150),random(0,200),10);
+  stroke(0,0,0);
+  strokeWeight(5);
   ellipse(x++,200,40,40);
+  ellipse(y--,200,40,40);
+  ellipse(200,a++,40,40);
+  ellipse(200,b--,40,40);
+  
+  ellipse(d--,e++,40,40);
+  ellipse(f++,g--,40,40);
+  ellipse(h--,i--,40,40);
+  ellipse(c++,c++,40,40);
+  if(c>400){
+    noLoop();
+  }
+  if(h<0){
+    h=400;
+  }
+  
   if(x>400){
     noLoop();
   }
-  ellipse(y--,200,40,40);
+  
+  if(y>400){
+    noLoop()
+  }
+  
+  
 }
