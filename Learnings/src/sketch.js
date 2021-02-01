c559f236756cd4ddf0a71d148dd4c5fca48022a3
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-01 20:57:33 +0600

    Objects

--- Learnings/src/sketch.js
+++ Learnings/src/sketch.js
@@ -1,5 +1,14 @@
-var circleX=0;
-var circleY=0;
+var circle1 = {
+  x:200,
+  y:50,
+  diameter:55
+};
+
+var circle2 = {
+  x:150,
+  y:200,
+  diameter:55
+};
 
 function setup()
 {
@@ -13,12 +22,14 @@ function draw()
 
   background(0,255,255)
   fill(255,121,1,180);
-  ellipse(circleX,circleY,15,15);
-  circleX+=1;
-  circleY+=3;
-  if(circleY>=300){
-    circleY-=30;
-  }
+  ellipse(circle1.x,circle1.y,circle1.diameter);
+  
+  
+ 
+  fill(255,121,1,180);
+  ellipse(circle2.x,circle2.y,circle2.diameter);
+  
+
    
 }
 
