310f0b4292497ec7d0a5dee6c6c2753967461c48
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-02 01:09:50 +0600

    Initialising Bird class

---
+++ Learnings/src/bird.js
@@ -0,0 +1,10 @@
+class Bird {
+    constructor(){
+       this.x=200;
+       this.y=400;
+    }
+
+    show(){
+        ellipse(this.x, this.y, 50);
+    }
+}
--- Learnings/src/sketch.js
+++ Learnings/src/sketch.js
@@ -1,62 +1,14 @@
-var x=0;
-var speedX=5;
-var y=0;
-var speedY=5;
-var a=255;
-var b=0;
-var c=0;
-var e=255;
-var f=0;
-var g=0;
+let bird ;
 function setup() {
-  createCanvas(400, 400);
-  
-   
+  createCanvas(1000, 800);
+   bird=new Bird();
 }
 
 function draw() {
-  background(255);
-  
-  if(x>=width){
-    speedX=-5;
-    a=255;
-    b=255;
-    c=0;
-  }
+  background(125,214,247);
+  bird.show();
   
   
-  stroke(0,0,0);
-  fill(a,b,c);
-  ellipse(x,200,50,50);
-  
-  
-  
-  x+=speedX;
-  
-  if(x<=0){
-    speedX=5;
-    a=255;
-    b=0;
-    c=0;
-  }
-  stroke(0,0,0);
-  fill(e,f,g);
-  ellipse(200,y,50,50);
-  if(y>=height){
-    speedY=-5;
-    e=255;
-    f=0;
-    g=0;
-  }
-  
-  y+=speedY;
-  
-  if(y<=0){
-    speedY=5;
-    e=255;
-    f=255;
-    g=0;
-  }
   
   
   
