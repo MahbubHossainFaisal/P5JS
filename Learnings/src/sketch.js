cac86ed4f3ecd2b2ae8d80e5daca1fd5cff3d7e0
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-01 15:52:03 +0600

    Making ellipse moving with mouse hovering

--- Learnings/src/sketch.js
+++ Learnings/src/sketch.js
@@ -1,32 +1,32 @@
 function setup()
 {
     createCanvas(400,300)
+   background(0,255,255)
 }
 
 function draw()
 {
-    background(0,255,255)
-    
+   
     line(0,0,400,300)
   
     rectMode(RADIUS);
     
   //color functions
-    fill(255,255,0);
-   stroke(255,100,110)
-  strokeWeight(8)
-  rect(180, 138, 80, 60);
+  //   fill(255,255,0);
+  //  stroke(255,100,110)
+  // strokeWeight(8)
+  // rect(180, 138, 80, 60);
   
   
-  fill(255,121,1)
+  // fill(255,121,1,180)
+  // stroke(100,200,255)
+  // strokeWeight(2)
+  // ellipse(260,138,40,40)
+  
+  fill(255,121,1,180)
   stroke(100,200,255)
   strokeWeight(2)
-  ellipse(260,138,40,40)
-  
-  fill(255,121,1)
-  stroke(100,200,255)
-  strokeWeight(2)
-  ellipse(100,138,40,40)
+  ellipse(mouseX,mouseY,10,10) //make the ellipse work and draw while hovering mouse
   
     
     
