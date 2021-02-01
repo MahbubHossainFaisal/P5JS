d22b172b68ac9260eaf47c2a38583f8860721ac1
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-01 16:01:35 +0600

    mousePressed() function

--- Learnings/src/sketch.js
+++ Learnings/src/sketch.js
@@ -1,33 +1,22 @@
 function setup()
 {
     createCanvas(400,300)
-   background(0,255,255)
+    background(0,255,255)
+   
 }
 
 function draw()
 {
+
+  
+  noStroke();
+  fill(255,121,1,180);
+  ellipse(mouseX,mouseY,15,15);
    
-    line(0,0,400,300)
+}
+
+function mousePressed(){
   
-    rectMode(RADIUS);
-    
-  //color functions
-  //   fill(255,255,0);
-  //  stroke(255,100,110)
-  // strokeWeight(8)
-  // rect(180, 138, 80, 60);
-  
-  
-  // fill(255,121,1,180)
-  // stroke(100,200,255)
-  // strokeWeight(2)
-  // ellipse(260,138,40,40)
-  
-  fill(255,121,1,180)
-  stroke(100,200,255)
-  strokeWeight(2)
-  ellipse(mouseX,mouseY,10,10) //make the ellipse work and draw while hovering mouse
-  
-    
-    
+  background(0,255,255)
+   
 }
