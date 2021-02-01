7f000c1d4044602c96512f5c0279189539a70b67
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-01 22:24:59 +0600

    Another use of random function

--- Learnings/src/sketch.js
+++ Learnings/src/sketch.js
@@ -1,48 +1,19 @@
-var a=0;
-var b=400;
-var c=0;
-var d=400;
-var e=0;
-var f=0;
-var g=400;
-var h=400;
-var i=400;
-var x=0;
-var y=400;
+
 function setup() {
   createCanvas(400, 400);
+   background(0);
 }
 
 function draw() {
+  let x=random(0,400);
+  let y=random(0,400)
+ 
   
-  background(255);
-  
-  fill(random(0,150),random(0,200),10);
+  fill(255,210,111,80);
   stroke(0,0,0);
-  strokeWeight(5);
-  ellipse(x++,200,40,40);
-  ellipse(y--,200,40,40);
-  ellipse(200,a++,40,40);
-  ellipse(200,b--,40,40);
-  
-  ellipse(d--,e++,40,40);
-  ellipse(f++,g--,40,40);
-  ellipse(h--,i--,40,40);
-  ellipse(c++,c++,40,40);
-  if(c>400){
-    noLoop();
-  }
-  if(h<0){
-    h=400;
-  }
-  
-  if(x>400){
-    noLoop();
-  }
-  
-  if(y>400){
-    noLoop()
-  }
+  strokeWeight(2);
+  ellipse(x,y,10,10);
+ 
   
   
 }
