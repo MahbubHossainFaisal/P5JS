6c8f2e0ba278ceda582a53bf53a6e3feac160664
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-08 23:52:09 +0600

    Making the plane object

--- Learnings/src/Space Shooter/plane.js
+++ Learnings/src/Space Shooter/plane.js
@@ -1,5 +1,11 @@
 class Plane {
     constructor(){
-        
+        this.position = createVector(width/2, height/2);
+        this.r = 40;
+    }
+    show(){
+        fill(255,0,0)
+        translate(this.position.x, this.position.y)
+        triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
     }
 }
--- Learnings/src/Space Shooter/sketch.js
+++ Learnings/src/Space Shooter/sketch.js
@@ -1,9 +1,12 @@
+let plane
 function setup(){
 
     createCanvas(windowWidth,windowHeight)
-
+    
+    plane = new Plane();
 }
 
 function draw(){
     background(0)
+    plane.show()
 }
