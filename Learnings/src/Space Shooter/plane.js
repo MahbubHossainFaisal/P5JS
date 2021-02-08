0ba9b9cba7964718c55eb616dbd5b17f1806bb7c
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-09 00:07:23 +0600

    setRotation and turn function to  make the plane object rotate

--- Learnings/src/Space Shooter/plane.js
+++ Learnings/src/Space Shooter/plane.js
@@ -1,11 +1,22 @@
 class Plane {
     constructor(){
         this.position = createVector(width/2, height/2);
-        this.r = 40;
+        this.r = 30;
+        this.h = 0;
+        this.rotation = 0;
     }
     show(){
         fill(255,0,0)
         translate(this.position.x, this.position.y)
+        rotate(this.h+PI/2);
         triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
+    }
+    
+    setRotation(x){
+        this.rotation = x;
+    }
+
+    turn(){
+        this.h += this.rotation;
     }
 }
