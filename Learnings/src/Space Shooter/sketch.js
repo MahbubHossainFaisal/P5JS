eed987be555c05edc038ca5d6b0f3b6dda4f78e8
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-09 00:15:38 +0600

    making keyPressed function and keyReleased function to make the plane object rotating around it's own exis

--- Learnings/src/Space Shooter/sketch.js
+++ Learnings/src/Space Shooter/sketch.js
@@ -9,4 +9,17 @@ function setup(){
 function draw(){
     background(0)
     plane.show()
+    plane.turn()
+}
+
+function keyReleased(){
+    plane.setRotation(0);
+  }
+
+function keyPressed(){
+    if(keyCode == RIGHT_ARROW){
+        plane.setRotation(0.1)
+    } else if(keyCode == LEFT_ARROW){
+        plane.setRotation(-0.1)
+    }
 }
