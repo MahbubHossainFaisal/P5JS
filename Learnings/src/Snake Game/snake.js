d1c50b7059d62d27ace4a186626bc39b44470e59
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-17 00:09:15 +0600

    While eating a food the snake is now increasing in size

--- Learnings/src/Snake Game/snake.js
+++ Learnings/src/Snake Game/snake.js
@@ -1,16 +1,18 @@
-  
 class Snake {
     constructor() {
         this.x = 0;
         this.y = 0;
         this.xSpeed = 1;
         this.ySpeed = 0;
-        
+        this.total = 0;
+        this.tail = []
     }
     
     eat(pos) {
+        
         var d = dist(this.x,this.y,pos.x,pos.y)
         if(d === 0){
+            this.total=this.total+1;
             return true
         } else {
             return false
@@ -18,6 +20,13 @@ class Snake {
     }
 
     update() {
+        if(this.total === this.tail.length){
+            for(var i = 0 ; i<this.tail.length; i++){
+                this.tail[i] = this.tail[i+1]
+            }
+        }
+        this.tail[this.total-1] = createVector(this.x,this.y)
+
         this.x = this.x + this.xSpeed*scl
         this.y = this.y + this.ySpeed*scl
 
@@ -26,8 +35,13 @@ class Snake {
     }
 
     show() {
+        
         fill(255)
+        for(var i = 0 ; i<this.tail.length ; i++){
+            rect(this.tail[i].x,this.tail[i].y,scl,scl)
+        }
         rect(this.x,this.y,scl,scl)
+         
     }
 
     direction(a,b) {
