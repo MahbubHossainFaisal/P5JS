5861f5cd7b2dc5cf0bdb54dc4ab4557603e647dd
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-03-22 23:47:31 +0600

    separate function for axis calculation,creating mouseclick event for point generation inside the graph

--- Learnings/src/SGD/sketch.js
+++ Learnings/src/SGD/sketch.js
@@ -1,4 +1,6 @@
 
+
+let arr = []
 function setup() {
 
     let cnv = createCanvas(600, 600)
@@ -7,8 +9,28 @@ function setup() {
 
 function draw() {
 
-    background(220)
+    background(240)
+    axis()
 
+    for(let i = 0; i< arr.length; i++){
+        let x = map(arr[i].x,0,1,0,width)
+        let y = map(arr[i].y,0,1,height,0)
+        fill(0)
+        ellipse(x,y,10,10)
+    }
+
+
+}
+
+
+function mousePressed(){
+    let x = map(mouseX, 0, width , 0,1)
+    let y = map(mouseY, 0, height, 1 ,0)
+    let p = new Point(x,y)
+    console.log(p)
+    arr.push(p)
+}
+function axis(){
     let x1 = 0
     let x2 = 0
     let y1 = 1
@@ -42,9 +64,8 @@ function draw() {
         ym = map(i, 0, 1, height, 0)
         stroke(0)
         strokeWeight(2)
-        text(i.toFixed(1), xm, ym-3)
+        text(i.toFixed(1), xm, ym - 3)
         line(xm, ym, xm + width, ym)
-
     }
 
 }
